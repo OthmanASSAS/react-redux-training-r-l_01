26589d0178b52d7bf477fa1adcbbc04468cd6d32
Othman ASSAS <o.assas@gop.link>
2019-03-06 09:40:04 +0100

    boiler plate react-redux

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+require('../style.css')
+
+class App extends Component{
+    render(){
+        return ("React-redux boiler plate")
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import App from "./components/app";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {};
+const middleware = [thunk];
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      initialState,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
